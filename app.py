33924555e8750500752e97ceff9b7336bc4f9a75
Rio Siswandi <rioitse21@gmail.com>
2025-06-14 03:16:13 +0700

    Fix: moved CORS to top

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import os
 import requests
 
 app = Flask(__name__)
-
+CORS(app)
 # === DOWNLOAD MODEL JIKA BELUM ADA ===
 MODEL_PATH = 'meowid_model.h5'
 
@@ -65,7 +65,6 @@ def predict():
         return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
-    CORS(app)
     import os
     port = int(os.environ.get('PORT', 5000))
     app.run(debug=True, host='0.0.0.0', port=port)
